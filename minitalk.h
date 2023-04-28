8766102cc51546af18872c7c61072d05df576a80
yugo <yugo.hoshino1906@gmail.com>
2023-04-28 09:32:41 +0900

    update:clientで、引数のバリデーションOK

--- minitalk.h
+++ minitalk.h
@@ -6,15 +6,18 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 22:41:58 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/04/25 23:36:05 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/04/28 09:31:45 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_TALK_H
 # define MINI_TALK_H
+# define PID_MAX 4194304
+# define PID_MIN 1
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdbool.h>
+# include "./include_libft/libft.h"
 
 #endif
--- src/client.c
+++ src/client.c
@@ -6,14 +6,15 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 22:42:05 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/04/26 08:23:20 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/04/28 09:32:18 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
-# include <sys/types.h>
+#include <limits.h>
+#include <sys/types.h>
 
-static bool	is_validation_arg(int argc)
+static bool	is_validation_argc_count(int argc)
 {
 	if (argc != 3)
 		return (true);
@@ -21,11 +22,35 @@ static bool	is_validation_arg(int argc)
 		return (false);
 }
 
+static bool	is_validation_pid(char *argv1)
+{
+	size_t	i;
+	int		pid;
+
+	i = 0;
+	while (argv1[i])
+	{
+		if (argv1[i] < '0' || argv1[i] > '9')
+			return (true);
+		i++;
+	}
+	pid = ft_atoi(argv1);
+	if (pid < PID_MIN || pid > PID_MAX)
+		return (true);
+	else
+		return (false);
+}
+
 int	main(int argc, char **argv)
 {
+	int pid;
 	(void)argv;
 	// 引数が2つではない場合はプログラム終了
-	if (is_validation_arg(argc))
+	if (is_validation_argc_count(argc))
 		exit(EXIT_FAILURE);
+	if (is_validation_pid(argv[1]))
+		exit(EXIT_FAILURE);
+
+	pid = ft_atoi(argv[1]);
 	return (1);
 }
