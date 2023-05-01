33d8395234d651651b095d33aa3de2104bf7db40
yugo <yugo.hoshino1906@gmail.com>
2023-05-01 09:55:35 +0900

    update:clientとserverで通信可能な状態に変更

--- src/client.c
+++ src/client.c
@@ -6,19 +6,18 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 22:42:05 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/04/28 09:32:18 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/05/01 09:54:18 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
-#include <limits.h>
-#include <sys/types.h>
 
 static bool	is_validation_argc_count(int argc)
 {
 	if (argc != 3)
 	{
-		ft_putstr_fd("Error_minitalk\n", STDERR_FILENO);
+		ft_putstr_fd("Error, Usage: ./client <server_pid> <message>\n",
+						STDERR_FILENO);
 		return (ERROR);
 	}
 	else
@@ -50,16 +49,52 @@ static bool	is_validation_pid(char *argv1)
 		return (NOT_ERROR);
 }
 
+static void	signal_handler_usr1_end(int signum)
+{
+	if (signum == SIGUSR1)
+	{
+		exit(EXIT_SUCCESS);
+	}
+}
+
+static void	send_signal(int server_pid, char *str)
+{
+	size_t	len;
+	size_t	i;
+	int		bit;
+
+	i = 0;
+	len = ft_strlen(str) + 1;
+	while (len-- > 0)
+	{
+		bit = 8;
+		while (bit-- > 0)
+		{
+			usleep(100);
+			if ((str[i] >> bit) & 1)
+				kill((pid_t)server_pid, SIGUSR2);
+			else
+				kill((pid_t)server_pid, SIGUSR1);
+		}
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	int pid;
-	(void)argv;
+	struct sigaction	s_sa;
+	int					server_pid;
+
 	// 引数が2つではない場合はプログラム終了
 	if (is_validation_argc_count(argc))
 		exit(EXIT_FAILURE);
 	if (is_validation_pid(argv[1]))
 		exit(EXIT_FAILURE);
-
-	pid = ft_atoi(argv[1]);
+	// シグナルハンドラーの設定_server.cからSIGUSR1を受け取ったらプロセスを終了
+	s_sa.sa_handler = signal_handler_usr1_end;
+	// シグナルハンドラーの設定を反映
+	sigaction(SIGUSR1, &s_sa, NULL);
+	server_pid = ft_atoi(argv[1]);
+	send_signal(server_pid, argv[2]);
 	return (1);
 }
--- src/server.c
+++ src/server.c
@@ -6,17 +6,51 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 23:32:20 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/04/30 23:31:59 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/05/01 09:54:26 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-int main(void)
+static void	signal_handler(int signum, siginfo_t *info, void *context)
 {
-	pid_t pid;
-	pid = getpid();
-	ft_printf("%d\n", pid);
+	static int i = 0;
+	(void)context;
+	(void)info;
+	// テスト：シグナルを受け取って出力できるか確認
+	if (signum == SIGUSR1)
+	{
+		ft_printf("%d\n", i);
+		i++;
+		ft_printf("SIGUSR1\n");
+	}
+	else if (signum == SIGUSR2)
+	{
+		ft_printf("%d\n", i);
+		i++;
+		ft_printf("SIGUSR2\n");
+	}
+	else
+	{
+		ft_printf("Error_minitalk\n");
+	}
+}
 
+// function_sigemptyset: Initialize the signal mask so that it does not block on signals received while running the signal_handler.
+
+int	main(void)
+{
+	struct sigaction	s_sa;
+	pid_t				pid;
+
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	s_sa.sa_sigaction = signal_handler;
+	s_sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&s_sa.sa_mask);
+	sigaction(SIGUSR1, &s_sa, NULL);
+	sigaction(SIGUSR2, &s_sa, NULL);
+	while(1)
+		pause();
 	return (0);
 }
